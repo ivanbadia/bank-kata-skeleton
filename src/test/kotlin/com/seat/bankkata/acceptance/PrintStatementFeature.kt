ff8f7682ed97dc83419f8ab5bd6dd5be028f66ed
Ivan Badia <ivanbadia@gmail.com>
2024-01-18 11:22:53 +0100

    modify acceptance test to use mockk rather than mockito

--- src/test/kotlin/com/seat/bankkata/acceptance/PrintStatementFeature.kt
+++ src/test/kotlin/com/seat/bankkata/acceptance/PrintStatementFeature.kt
@@ -2,21 +2,16 @@ package com.seat.bankkata.acceptance
 
 import com.seat.bankkata.AccountService
 import com.seat.bankkata.Clock
+import io.mockk.every
+import io.mockk.mockk
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.BDDMockito.given
-import org.mockito.Mock
-import org.mockito.junit.jupiter.MockitoExtension
 import java.io.ByteArrayOutputStream
 import java.io.PrintStream
-import java.time.LocalDate
 
 
-@ExtendWith(MockitoExtension::class)
 class PrintStatementsFeature {
-    @Mock
     private lateinit var clock: Clock
 
     private lateinit var account: AccountService
@@ -25,17 +20,18 @@ class PrintStatementsFeature {
 
     @BeforeEach
     fun setUp() {
+        clock = mockk<Clock>()
         account = AccountService(clock)
         redirectConsoleOutputTo(output)
     }
 
     @Test
     fun `print statement contains all transactions in descending order`() {
-        given(clock.today()).willReturn("01/04/2014")
+        every {  clock.today() } returns "01/04/2014"
         account.deposit(1000)
-        given(clock.today()).willReturn("02/04/2014")
+        every {  clock.today() } returns "02/04/2014"
         account.withdraw(100)
-        given(clock.today()).willReturn("10/04/2014")
+        every {  clock.today() } returns "10/04/2014"
         account.deposit(500)
 
         account.printStatement()
